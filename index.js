f257bc749e61c1d00307f67e5a313fbb6a5b61a5
ahmed imran <ahmed.eng2709@gmail.com>
2022-07-16 14:33:03 +0500

    Result calculater

---
+++ index.js
@@ -0,0 +1,53 @@
+function  Resultcalculator(){
+    
+    let subject1Marks = +document.querySelector("#subject1Marks").value;
+    let subject2Marks = +document.querySelector("#subject2Marks").value;
+    let subject3Marks = +document.querySelector("#subject3Marks").value;
+    let subject4Marks = +document.querySelector("#subject4Marks").value;
+    let subject5Marks = +document.querySelector("#subject5Marks").value;
+    let totalmarks = +document.querySelector("#totalmarks").value;
+   
+    let totalSubject1Markspercentage = 100 * subject1Marks / (totalmarks/5) ;
+    let totalSubject2Markspercentage = 100 * subject2Marks / (totalmarks/5) ;
+    let totalSubject3Markspercentage = 100 * subject3Marks / (totalmarks/5) ;
+    let totalSubject4Markspercentage = 100 * subject4Marks / (totalmarks/5) ;
+    let totalSubject5Markspercentage = 100 * subject5Marks / (totalmarks/5) ;
+  
+    let totalSubjectMarks = subject1Marks + subject2Marks + subject3Marks + subject4Marks + subject5Marks;
+    document.querySelector("#totalSubjectMarks").innerHTML = `Total Subject-Marks :: ${totalSubjectMarks} out of ${totalmarks}`;
+    
+   
+   document.querySelector("#totalSubject1Markspercentage").innerHTML = `Subject1-Markspercentage :: ${totalSubject1Markspercentage}%`;
+   document.querySelector("#totalSubject2Markspercentage").innerHTML = `Subject2-Markspercentage :: ${totalSubject2Markspercentage}%`;
+   document.querySelector("#totalSubject3Markspercentage").innerHTML = `Subject3-Markspercentage :: ${totalSubject3Markspercentage}%`;
+   document.querySelector("#totalSubject4Markspercentage").innerHTML = `Subject4-Markspercentage :: ${totalSubject4Markspercentage}%`;  
+   document.querySelector("#totalSubject5Markspercentage").innerHTML = `Subject5-Markspercentage :: ${totalSubject5Markspercentage}%`;
+  
+   let totalSubjectMarkspercentage =  totalSubjectMarks * 100 / totalmarks;
+  
+    document.querySelector("#totalSubjectMarkspercentage").innerHTML = `percentage :: ${totalSubjectMarkspercentage}% `;
+    
+  
+    if (totalSubjectMarkspercentage  >= 80 && totalSubjectMarkspercentage  <=  100 ){
+          document.querySelector("#Grade").innerHTML = `GRADE A1`;
+    }
+    else if (totalSubjectMarkspercentage >= 70 && totalSubjectMarkspercentage <= 80 ){
+          document.querySelector("#Grade").innerHTML = `GRADE :: A`;
+    }
+    else if (totalSubjectMarkspercentage >= 60 && totalSubjectMarkspercentage <= 70 ){
+          document.querySelector("#Grade").innerHTML = `GRADE :: B`;
+    }
+    else if (totalSubjectMarkspercentage >= 50 && totalSubjectMarkspercentage <= 60 ){
+          document.querySelector("#Grade").innerHTML = `GRADE :: C`;
+    }
+    else if (totalSubjectMarkspercentage >= 45 && totalSubjectMarkspercentage <= 50 ){
+          document.querySelector("#Grade").innerHTML = `GRADE :: D`;
+    }
+    else if (totalSubjectMarkspercentage >= 40 && totalSubjectMarkspercentage <= 45 ){
+          document.querySelector("#Grade").innerHTML = `GRADE :: E`;
+    }
+  
+    else{
+          document.querySelector("#Grade").innerHTML = `GRADE :: F`;
+    }
+    }
