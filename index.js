e5e9ee0c09fcb108a10b0fad2bbcdbe5b85d053c
ahmed imran <ahmed.eng2709@gmail.com>
2022-07-18 16:09:24 +0500

    add new founction show all subject marks in screen

--- index.js
+++ index.js
@@ -1,32 +1,76 @@
 function  Resultcalculator(){
-    //input subject marks 
+    //input-mathed subject marks 
     let subject1Marks = +document.querySelector("#subject1Marks").value;
     let subject2Marks = +document.querySelector("#subject2Marks").value;
     let subject3Marks = +document.querySelector("#subject3Marks").value;
     let subject4Marks = +document.querySelector("#subject4Marks").value;
     let subject5Marks = +document.querySelector("#subject5Marks").value;
     let totalmarks = +document.querySelector("#totalmarks").value;
-   // single subject marks percentage calculater
-    let totalSubject1Markspercentage = 100 * subject1Marks / (totalmarks/5) ;
-    let totalSubject2Markspercentage = 100 * subject2Marks / (totalmarks/5) ;
-    let totalSubject3Markspercentage = 100 * subject3Marks / (totalmarks/5) ;
-    let totalSubject4Markspercentage = 100 * subject4Marks / (totalmarks/5) ;
-    let totalSubject5Markspercentage = 100 * subject5Marks / (totalmarks/5) ;
-  
+
+if(subject1Marks>100 || subject2Marks>100 || subject3Marks>100 || subject4Marks>100 || subject5Marks>100 )        {
+      alert("please enter correct value");
+}
+
+    document.querySelector("#ObtaniendSubjectmarks1").innerHTML = `${subject1Marks}`;
+    document.querySelector("#ObtaniendSubjectmarks2").innerHTML = `${subject2Marks}`;
+    document.querySelector("#ObtaniendSubjectmarks3").innerHTML = `${subject3Marks}`;
+    document.querySelector("#ObtaniendSubjectmarks4").innerHTML = `${subject4Marks}`;
+    document.querySelector("#ObtaniendSubjectmarks5").innerHTML = `${subject5Marks}`;
+
+
+
+    //All subject marks add founction
     let totalSubjectMarks = subject1Marks + subject2Marks + subject3Marks + subject4Marks + subject5Marks;
-    //subject marks print function
+    //All subject marks print function
     document.querySelector("#totalSubjectMarks").innerHTML = `Total Subject-Marks :: ${totalSubjectMarks} out of ${totalmarks}`;
     
-   
-   document.querySelector("#totalSubject1Markspercentage").innerHTML = `Subject1-Markspercentage :: ${totalSubject1Markspercentage}%`;
-   document.querySelector("#totalSubject2Markspercentage").innerHTML = `Subject2-Markspercentage :: ${totalSubject2Markspercentage}%`;
-   document.querySelector("#totalSubject3Markspercentage").innerHTML = `Subject3-Markspercentage :: ${totalSubject3Markspercentage}%`;
-   document.querySelector("#totalSubject4Markspercentage").innerHTML = `Subject4-Markspercentage :: ${totalSubject4Markspercentage}%`;  
-   document.querySelector("#totalSubject5Markspercentage").innerHTML = `Subject5-Markspercentage :: ${totalSubject5Markspercentage}%`;
-  
+     
+   // All subject marks percentage calculate function
    let totalSubjectMarkspercentage =  totalSubjectMarks * 100 / totalmarks;
+   // All subject marks percentage print function
+    document.querySelector("#totalSubjectMarkspercentage").innerHTML = ` ${totalSubjectMarkspercentage}% `;
+
+
+  // single subject marks calculate function
+    let singleSubjectmarks = totalmarks/5;
+    // print single subject marks
+     document.querySelector("#singleSubjectmarks1").innerHTML = `${singleSubjectmarks}`;
+     document.querySelector("#singleSubjectmarks2").innerHTML = `${singleSubjectmarks}`;
+     document.querySelector("#singleSubjectmarks3").innerHTML = `${singleSubjectmarks}`;
+     document.querySelector("#singleSubjectmarks4").innerHTML = `${singleSubjectmarks}`;
+     document.querySelector("#singleSubjectmarks5").innerHTML = `${singleSubjectmarks}`;
+
+
+
+
+
+     // single subject marks percentage calculater founction
+   let totalSubject1Markspercentage = 100 * subject1Marks / singleSubjectmarks;   
+   let totalSubject2Markspercentage = 100 * subject2Marks / singleSubjectmarks;
+   let totalSubject3Markspercentage = 100 * subject3Marks / singleSubjectmarks;
+   let totalSubject4Markspercentage = 100 * subject4Marks / singleSubjectmarks;
+   let totalSubject5Markspercentage = 100 * subject5Marks / singleSubjectmarks;
+
+// single subject marks percentage print fouction
+   document.querySelector("#totalSubject1Markspercentage").innerHTML = `${totalSubject1Markspercentage}%`;
+   document.querySelector("#totalSubject2Markspercentage").innerHTML = `${totalSubject2Markspercentage}%`;
+   document.querySelector("#totalSubject3Markspercentage").innerHTML = `${totalSubject3Markspercentage}%`;
+   document.querySelector("#totalSubject4Markspercentage").innerHTML = `${totalSubject4Markspercentage}%`;  
+   document.querySelector("#totalSubject5Markspercentage").innerHTML = `${totalSubject5Markspercentage}%`;
   
-    document.querySelector("#totalSubjectMarkspercentage").innerHTML = `percentage :: ${totalSubjectMarkspercentage}% `;
+
+
+
+
+
+
+
+
+
+
+ 
+
+
     //condition check for student awarded grade
   
     if (totalSubjectMarkspercentage  >= 80 && totalSubjectMarkspercentage  <=  100 ){
